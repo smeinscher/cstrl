0a185431c97b20557b6772949a989453948fe790
Sterling Meinscher <sterling@meinscher.com>
2026-06-22 08:41:06 -0500

    gltf loader fixes

--- src/model/gltf_loader.c
+++ src/model/gltf_loader.c
@@ -63,6 +63,91 @@ CSTRL_API scene_t cstrl_model_generate_scene_from_gltf_file(const char *path)
     scene.name = scene_internal->data->scene->name;
     scene.model_count = scene_internal->data->nodes_count;
     scene.models = malloc(sizeof(model_t) * scene.model_count);
+
+    int position_index = -1;
+    int uv_index = -1;
+    int indices_index = -1;
+    for (int i = 0; i < scene_internal->data->meshes[0].primitives[0].attributes_count; i++)
+    {
+        if (strcmp(scene_internal->data->meshes[0].primitives[0].attributes[i].name, "POSITION") == 0)
+        {
+            position_index = i;
+            continue;
+        }
+        if (strcmp(scene_internal->data->meshes[0].primitives[0].attributes[i].name, "TEXCOORD_0") == 0)
+        {
+            uv_index = i;
+            continue;
+        }
+    }
+    if (scene_internal->data->meshes[0].primitives[0].indices != NULL)
+    {
+        indices_index = scene_internal->data->meshes[0].primitives[0].attributes_count;
+    }
+
+    for (int i = 0; i < scene_internal->data->buffer_views_count; i++)
+    {
+        size_t offset = scene_internal->data->buffer_views[i].offset;
+        size_t size = scene_internal->data->buffer_views[i].size;
+        unsigned char *buffer = scene_internal->data->buffers[0].data;
+        // unsigned char *buffer = (unsigned char *)scene_internal->data->buffer_views[i].buffer;
+        if (i == position_index)
+        {
+            float *new_buffer = malloc(size);
+            memcpy(new_buffer, buffer + offset, size);
+            cstrl_renderer_add_positions(scene.render_datum[0], new_buffer, 3, size / 12, CSTRL_USAGE_DYNAMIC);
+            da_float colors;
+            cstrl_da_float_init(&colors, size);
+            for (int j = 0; j < size / 12; j++)
+            {
+                cstrl_da_float_push_back(&colors, 1.0f);
+                cstrl_da_float_push_back(&colors, 1.0f);
+                cstrl_da_float_push_back(&colors, 1.0f);
+                cstrl_da_float_push_back(&colors, 1.0f);
+            }
+            cstrl_renderer_add_colors(scene.render_datum[0], colors.array, CSTRL_USAGE_STATIC);
+            cstrl_da_float_free(&colors);
+            free(new_buffer);
+        }
+        else if (i == uv_index)
+        {
+            float *new_buffer = malloc(size);
+            memcpy(new_buffer, buffer + offset, size);
+            cstrl_renderer_add_uvs(scene.render_datum[0], new_buffer, CSTRL_USAGE_STATIC);
+            free(new_buffer);
+        }
+        else if (i == indices_index)
+        {
+            int *new_buffer = malloc(size / 2 * sizeof(int));
+            short *short_buffer = (short *)buffer;
+            for (int j = 0; j < size / 2; j++)
+            {
+                new_buffer[j] = short_buffer[j + offset / 2];
+            }
+            cstrl_renderer_add_indices(scene.render_datum[0], new_buffer, size / 2, CSTRL_USAGE_STATIC);
+            free(new_buffer);
+        }
+    }
+    return scene;
+}
+
+static scene_t old(const char *path)
+{
+    scene_t scene = {0};
+    scene.scene_internal = malloc(sizeof(scene_internal_t));
+    scene_internal_t *scene_internal = scene.scene_internal;
+    scene_internal->data = cstrl_model_load_gltf_file(path);
+    if (scene_internal->data == NULL)
+    {
+        log_error("Failed to load scene");
+        return (scene_t){0};
+    }
+    scene.render_datum = malloc(sizeof(cstrl_render_data));
+    scene.render_datum[0] = cstrl_renderer_create_render_data();
+    scene.render_data_count = 1;
+    scene.name = scene_internal->data->scene->name;
+    scene.model_count = scene_internal->data->nodes_count;
+    scene.models = malloc(sizeof(model_t) * scene.model_count);
     da_float positions;
     cstrl_da_float_init(&positions, 32);
     da_float colors;
@@ -107,7 +192,7 @@ CSTRL_API scene_t cstrl_model_generate_scene_from_gltf_file(const char *path)
             {
                 unsigned short *index =
                     &primitive.indices->buffer_view->buffer->data[indices_offset + k * indices_stride];
-                log_debug("Index: %hu", *index);
+                // log_debug("Index: %hu", *index);
                 cstrl_da_int_push_back(&indices, *index);
             }
             for (int k = 0; k < primitive.attributes_count; k++)
@@ -121,8 +206,8 @@ CSTRL_API scene_t cstrl_model_generate_scene_from_gltf_file(const char *path)
                     {
                         cgltf_size stride = attribute.data->buffer_view->stride * l;
                         cgltf_size offset = attribute.data->offset / sizeof(float);
-                        log_debug("Position: %f %f %f", data[l * 3 + stride + offset],
-                                  data[l * 3 + 1 + stride + offset], data[l * 3 + 2 + stride + offset]);
+                        // log_debug("Position: %f %f %f", data[l * 3 + stride + offset],
+                        //           data[l * 3 + 1 + stride + offset], data[l * 3 + 2 + stride + offset]);
                         cstrl_da_float_push_back(&positions, data[l * 3 + stride + offset]);
                         cstrl_da_float_push_back(&positions, data[l * 3 + stride + 1 + offset]);
                         cstrl_da_float_push_back(&positions, data[l * 3 + stride + 2 + offset]);
@@ -143,8 +228,8 @@ CSTRL_API scene_t cstrl_model_generate_scene_from_gltf_file(const char *path)
     }
 
     cstrl_renderer_add_positions(scene.render_datum[0], positions.array, 3, positions.size / 3, CSTRL_USAGE_DYNAMIC);
-    cstrl_renderer_add_colors(scene.render_datum[0], colors.array, CSTRL_USAGE_DYNAMIC);
-    cstrl_renderer_add_indices(scene.render_datum[0], indices.array, indices.size, CSTRL_USAGE_DYNAMIC);
+    cstrl_renderer_add_colors(scene.render_datum[0], colors.array, CSTRL_USAGE_STATIC);
+    cstrl_renderer_add_indices(scene.render_datum[0], indices.array, indices.size, CSTRL_USAGE_STATIC);
 
     cstrl_da_float_free(&positions);
     cstrl_da_float_free(&colors);
